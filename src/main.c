16f44ea677205d3bfa0887346f7a4b9dbb67d21f
Phanty133 <48225206+Phanty133@users.noreply.github.com>
2023-02-20 19:45:25 +0100

    Restructured main execution

--- src/main.c
+++ src/main.c
@@ -3,13 +3,24 @@
 #include "uart.h"
 #include "drivers/ili9341/lcd_setup.h"
 #include "drivers/ili9341/lcd_tests.h"
+#include "delay.h"
 
-int main() {
+void setup() {
 	uart_begin(115200);
 	lcd_init();
 	lcd_select();
 
 	lcd_test_all();
+}
+
+void loop() {
+	uart_write_line("Hello world!");
+	delay_milli(1000);
+}
+
+int main() {
+	setup();
+	while (1) loop();
 
 	return 0;
 }
