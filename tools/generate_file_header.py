f226d02acc2e0c132d2bc2542f984f45ae23b0b4
Phanty133 <48225206+Phanty133@users.noreply.github.com>
2023-02-25 11:06:37 +0100

    Updated file generator to support QUICK_UPLOAD

--- tools/generate_file_header.py
+++ tools/generate_file_header.py
@@ -16,26 +16,38 @@ input_name = os.path.basename(input_path)
 
 output_name = f"{input_name}.h"
 output_path = os.path.join(output_dir, output_name)
-output_data = ""
 
 file_ext = input_name.split(".")[-1]
 file_name = "".join(input_name.split(".")[:-1])
 arr_name = file_name.upper()
-output_data += f"#ifndef __{arr_name}_{file_ext.upper()}_H\n"
-output_data += f"#define __{arr_name}_{file_ext.upper()}_H\n"
-output_data += f"#include <stdint.h>\n"
-output_data += f"static const uint8_t {arr_name}[] = {{"
+
+output_template = """\
+#ifndef __{arr_name}_{ext}_H
+#define __{arr_name}_{ext}_H
+#include <stdint.h>
+#ifdef QUICK_UPLOAD
+static const uint8_t {arr_name}[] = {{}};
+#else
+static const uint8_t {arr_name}[] = {{{data}}};
+#endif
+#endif
+"""
+
+format_params = {
+	"arr_name": arr_name,
+	"ext": file_ext.upper(),
+	"data": ""
+}
 
 with open(input_path, "rb") as file:
 	file_data = file.read()
 
 	for char in file_data[:-1]:
-		output_data += f"{hex(char)},"
+		format_params["data"] += f"{hex(char)},"
 
-	output_data += f"{hex(file_data[-1])}"
+	format_params["data"] += f"{hex(file_data[-1])}"
 
-output_data += "};\n"
-output_data += "#endif"
+output_data = output_template.format(**format_params)
 
 with open(output_path, "w") as file:
 	file.write(output_data)
